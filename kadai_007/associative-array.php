6fff4b9115c2e2f842cab53b3ed16118707bfd85
kobucyacya <i_ran_i_ran@icloud.com>
2024-08-21 13:52:35 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <p>
+        <?php
+        //連想配列を作成し、変数に代入
+        $merchandise_data = ['name'=>'onion', 'price'=>200, 'weight'=>160];
+        //print_r関数を使いブラウザに連想配列を出力
+        print_r($merchandise_data);
+        ?>
+    </p>
+</body>
+</html>
+
+
+
